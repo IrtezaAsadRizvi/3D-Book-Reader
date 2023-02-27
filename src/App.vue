b39c4def8eac6b1a56766e76ec67778d49228038
Irteza Asad <irtezaasadrizvi@gmail.com>
2023-02-27 18:36:48 +0600

    Load pdf to book

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,118 @@
 <template>
+
+  <input type="file" accept="application/pdf" name="file" id="FileInput" v-on:change="uploadFile">
+  {{ uploadProgress }}
+  
+
   <div class="book-container">
     <div class="book">
-      <div id="pages" class="pages">
-        <div class="page"><p>Open Me, <br>please!</p></div>
-        <div class="page"></div>
-        <div class="page"><p>Hello there!</p></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-        <div class="page"></div>
-      </div>
+      <div id="pages" class="pages hidden"></div>
     </div>
   </div>
 </template>
 
-
-<script setup>
-import {  onMounted } from 'vue'
-onMounted(() => {
-  var pages = document.getElementsByClassName('page');
-for(var i = 0; i < pages.length; i++)
-{
-  var page = pages[i];
-  if (i % 2 === 0)
-    {
-      page.style.zIndex = (pages.length - i);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-  for(var i = 0; i < pages.length; i++)
-    {
-      //Or var page = pages[i];
-      pages[i].pageNum = i + 1;
-      pages[i].onclick=function()
-        {
-          if (this.pageNum % 2 === 0)
-            {
+<script>
+  export default {
+    data: () => ({
+      bookFile: null,
+      uploadProgress: 0
+    }),
+    mounted () {
+      // document.addEventListener('DOMContentLoaded', function () {
+      //   this.initiatePages()
+      // })
+    },
+    methods: {
+      initiatePages () {
+        var pages = document.getElementsByClassName('page');
+        for (var i = 0; i < pages.length; i++) {
+          var page = pages[i];
+          if (i % 2 === 0) {
+            page.style.zIndex = (pages.length - i);
+          }
+        }
+        for (var i = 0; i < pages.length; i++) {
+          //Or var page = pages[i];
+          pages[i].pageNum = i + 1;
+          pages[i].onclick = function () {
+            if (this.pageNum % 2 === 0) {
               this.classList.remove('flipped');
               this.previousElementSibling.classList.remove('flipped');
             }
-          else
-            {
+            else {
               this.classList.add('flipped');
               this.nextElementSibling.classList.add('flipped');
             }
           }
+        }
+      },
+      async pdfToImages(pdfFile) {
+        const file = pdfFile
+        const fileReader = new FileReader();
+        const pages = document.getElementById('pages');
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+        const ratio = (vw/1440) * 0.8
+
+        return new Promise((resolve, reject) => {
+          try {
+            fileReader.onload = () => {
+            const arrayBuffer = fileReader.result;
+            pdfjsLib.getDocument(arrayBuffer).promise.then((pdf) => {
+              for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
+                
+                pdf.getPage(pageNumber).then((page) => {
+                  var pageElem = document.createElement('div');
+                  pageElem.classList.add('page');
+                  pages.appendChild(pageElem);
+
+                  const pageWidth = pageElem.getBoundingClientRect().width
+                  const pageHeight = pageElem.getBoundingClientRect().height
+
+
+                  const viewport = page.getViewport({ scale: ratio });
+                  const canvas = document.createElement('canvas');
+                  const context = canvas.getContext('2d');
+
+                  canvas.width = pageWidth;
+                  canvas.height = pageHeight;
+
+                  const renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                  };
+
+                  page.render(renderContext).promise.then(() => {
+                    const preview = document.createElement("div");
+                    pageElem.appendChild(preview);
+                    preview.appendChild(canvas);
+                    preview.classList.add('preview');
+                    this.uploadProgress = Math.round((pageNumber/pdf.numPages) * 100)
+                    if (pageNumber == pdf.numPages) {
+                      resolve()
+                    }
+                  }).catch((error) => {
+                    reject(error)
+                  });
+                }).catch((error) => {
+                  reject(error)
+                });
+              }
+            }).catch((error) => {
+              reject(error)
+            });
+          };
+
+          fileReader.readAsArrayBuffer(file);
+          } catch (error) {
+            reject(error)
+          }
+        })
+      },
+      async uploadFile(event) {
+        this.bookFile = event.target.files[0];
+        await this.pdfToImages(this.bookFile)
+        this.initiatePages()
       }
-})
-
-async function pdfToImages(pdfFile) {
-  // Load the PDF file using pdf.js
-  const pdf = await pdfjsLib.getDocument(pdfFile).promise;
-
-  // Initialize an array to hold the resulting images
-  const images = [];
-
-  // Loop through each page of the PDF
-  for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-    // Get the page object for the current page
-    const page = await pdf.getPage(pageNum);
-
-    // Set the desired image resolution (in DPI)
-    const scale = 2;
-
-    // Get the viewport object for the current page
-    const viewport = page.getViewport({ scale });
-
-    // Create a canvas element to draw the page onto
-    const canvas = document.createElement("canvas");
-
-    // Set the dimensions of the canvas to match the viewport
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
-
-    // Get the canvas context
-    const context = canvas.getContext("2d");
-
-    // Render the page onto the canvas
-    await page.render({ canvasContext: context, viewport }).promise;
-
-    // Convert the canvas to an image data URL
-    const imageDataUrl = canvas.toDataURL();
-
-    // Create a new Image object and set its src to the data URL
-    const image = new Image();
-    image.src = imageDataUrl;
-
-    // Push the image onto the array of images
-    images.push(image);
+    }
   }
-
-  // Return the array of images
-  return images;
-}
-
-})
-
 </script>
